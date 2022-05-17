8b15953e8a8fdf1e12ae5d127175fdb5d1266c0a
Nishant2009 <92501994+Nishant2009@users.noreply.github.com>
2022-05-17 08:51:30 +0530

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='lolcat',
+      version='7.0',
+      description='lolcat without newline character',
+      author='NISHANT',
+      author_telegram='https://t.me/lets_do_hacking',
+      url='https://github.com/Nishant2009/lolcat/',
+      scripts=['lolcat'],
+     )
